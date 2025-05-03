c277c7c32c9d92a7e5f89cc0b25a51004ef85713
rayosdev <rayos-dev@gmail.com>
2025-05-03 14:03:39 +0200

    implemented feture movies fetch

--- app/infrastructure/repositories/movie.ts
+++ app/infrastructure/repositories/movie.ts
@@ -7,12 +7,14 @@ export async function findMoviesMatchingQuery(
   signal: AbortSignal,
   params: Partial<Movie>,
 ): Promise<Movie[]> {
+  
   const url = new URL('/movies', apiUrl);
 
   for (const [key, value] of Object.entries(params)) {
     url.searchParams.set(key, value.toString());
   }
 
+  
   const request = await fetch(url, { signal });
   if (!request.ok) return [];
 
@@ -36,6 +38,17 @@ export async function getMovieByIdQuery(
 export async function getFeaturedMoviesQuery(
   signal: AbortSignal,
 ): Promise<Movie[]> {
+  try {
+    const url = new URL('/movies/recommended', apiUrl)
+    const request = await fetch(url)
+
+    if(!request.ok) return []
+
+    const json = (await request.json()) as Movie[]
+    return json
+  } catch (error) {
+    
+  }
   return [];
 }
 
